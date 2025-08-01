6ec968446f369763e295a99352fa8786d8884236
yuji421 <ey130421@gmail.com>
2025-08-01 18:55:35 +0900

    Q10_DoubleToIntCast.java を更新
    コメントの追加

--- Q10_DoubleToIntCast.java
+++ Q10_DoubleToIntCast.java
@@ -8,7 +8,9 @@ public class Q10_DoubleToIntCast {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("実数を入力: ");
+        //double型の入力はnextDouble()を使う
         double d = sc.nextDouble();
+        //大きい型から小さい型はキャストする必要がある
         int i = (int) d;
         System.out.println("整数に変換した値: " + i);
         sc.close();
